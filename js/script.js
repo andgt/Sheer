a808e3d9e34ab88f66bd022d17f6de801ef71e61
Anatoly <anatolijdegtyar@yandex.ru>
2021-06-21 18:43:06 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -30,3 +30,36 @@ window.onscroll = function () {
 scrollUp.onclick = function (evt) {
   window.scrollTo(0, 0);
 };
+
+// Табы
+let tab = function() {
+  let tabBtn = document.querySelectorAll(".description__link");
+  let descriptionBlock = document.querySelectorAll(".description__inner");
+  let tabName;
+
+  tabBtn.forEach(element => {
+    element.addEventListener("click", selectTab)
+  });
+
+  function selectTab(evt) {
+    evt.preventDefault();
+    tabBtn.forEach(element => {
+      element.classList.remove("description__inner--tab");
+    });
+    this.classList.add("description__inner--tab");
+    tabName = this.getAttribute("data-tab");
+    selectTabContent(tabName);
+  }
+
+  function selectTabContent(tabName) {
+    descriptionBlock.forEach(element => {
+      if (element.classList.contains(tabName)) {
+        element.classList.add("description__inner--tab");
+      } else {
+        element.classList.remove("description__inner--tab");
+      }
+    });
+  };
+};
+
+tab();
